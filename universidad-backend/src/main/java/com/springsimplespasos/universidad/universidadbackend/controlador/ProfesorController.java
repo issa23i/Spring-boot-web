dc56f98f5ab5f0af289346c5d88ba353b8ea8623
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-02-14 17:33:21 +0100

    Ejercicios cierre de sección Spring Boot Web

--- universidad-backend/src/main/java/com/springsimplespasos/universidad/universidadbackend/controlador/ProfesorController.java
+++ universidad-backend/src/main/java/com/springsimplespasos/universidad/universidadbackend/controlador/ProfesorController.java
@@ -1,8 +1,10 @@
 package com.springsimplespasos.universidad.universidadbackend.controlador;
 
 import com.springsimplespasos.universidad.universidadbackend.exceptions.BadRequestException;
+import com.springsimplespasos.universidad.universidadbackend.modelo.entidades.Alumno;
 import com.springsimplespasos.universidad.universidadbackend.modelo.entidades.Carrera;
 import com.springsimplespasos.universidad.universidadbackend.modelo.entidades.Persona;
+import com.springsimplespasos.universidad.universidadbackend.modelo.entidades.Profesor;
 import com.springsimplespasos.universidad.universidadbackend.servicios.contratos.CarreraDAO;
 import com.springsimplespasos.universidad.universidadbackend.servicios.contratos.PersonaDAO;
 import com.springsimplespasos.universidad.universidadbackend.servicios.implementaciones.ProfesorDAOImpl;
@@ -10,8 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/profesores")
@@ -34,5 +38,23 @@ public class ProfesorController extends PersonaController {
         List<Persona> listaProfesores = (List<Persona>) profesores;
         if(listaProfesores.isEmpty()) throw new BadRequestException(String.format("No se encuentran profesores en la carrera con id %d", idCarrera));
         return profesores;
+    }
+
+    @PutMapping("/{idProfesor}/carrera/{idCarrera}")
+    public Persona asignarCarreraAProfesor (@PathVariable Integer idProfesor, @PathVariable Integer idCarrera){
+        Optional<Persona> optionalProfesor = service.findById(idProfesor);
+        if(optionalProfesor.isEmpty()) throw new BadRequestException(String.format("Profesor con id %d no existe", idProfesor));
+        Optional<Carrera> optionalCarrera = carreraDAO.findById(idCarrera);
+        if(optionalCarrera.isEmpty()) throw new BadRequestException(String.format("La carrera con id %d no existe", idCarrera));
+
+        Persona profesor = optionalProfesor.get();
+        Carrera carrera = optionalCarrera.get();
+
+        Set<Carrera> carrerasProfesor = ((Profesor) profesor).getCarreras();
+        if(carrerasProfesor.isEmpty()) carrerasProfesor = new HashSet<>();
+        carrerasProfesor.add(carrera);
+        ((Profesor) profesor).setCarreras(carrerasProfesor);
+
+        return service.save(profesor);
     }
 }

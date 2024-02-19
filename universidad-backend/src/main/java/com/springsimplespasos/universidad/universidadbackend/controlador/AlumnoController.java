52ec9d0fc3b5b1241b9bc786e582d30c0062b17b
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-02-19 13:41:03 +0100

    Mejoras SonarLint

--- universidad-backend/src/main/java/com/springsimplespasos/universidad/universidadbackend/controlador/AlumnoController.java
+++ universidad-backend/src/main/java/com/springsimplespasos/universidad/universidadbackend/controlador/AlumnoController.java
@@ -1,6 +1,6 @@
 package com.springsimplespasos.universidad.universidadbackend.controlador;
 
-import com.springsimplespasos.universidad.universidadbackend.exceptions.BadRequestException;
+
 import com.springsimplespasos.universidad.universidadbackend.modelo.entidades.Alumno;
 import com.springsimplespasos.universidad.universidadbackend.modelo.entidades.Carrera;
 import com.springsimplespasos.universidad.universidadbackend.modelo.entidades.Persona;
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -20,7 +19,6 @@ import java.util.Optional;
 @RequestMapping("/alumnos")
 public class AlumnoController extends PersonaController {
 
-    //private final PersonaDAO alumnoDAO;
     private final CarreraDAO carreraDAO;
 
     @Autowired
@@ -30,66 +28,18 @@ public class AlumnoController extends PersonaController {
         this.carreraDAO = carreraDAO;
     }
 
-/*
-
-    @Autowired
-    public AlumnoController(PersonaDAO alumnoDAO, CarreraDAO carreraDAO) {
-        this.alumnoDAO = alumnoDAO;
-        this.carreraDAO = carreraDAO;
-    }
-
-
-    @PostMapping
-    public Persona altaAlumno(@RequestBody Persona alumno){
-        return alumnoDAO.save(alumno);
-    }
-
-    @GetMapping("/{id}")
-    public Persona obtenerAlumnoPorId(@PathVariable(required = false) Integer id){
-        Optional<Persona> optionalAlumno = alumnoDAO.findById(id);
-        if(optionalAlumno.isEmpty()) throw new BadRequestException(String.format("Alumno con id %d no existe", id));
-        return optionalAlumno.get();
-    }
-
-    @GetMapping
-    public List<Persona> obtenerTodos(){
-        List<Persona> alumnos = (List<Persona>) alumnoDAO.findAll();
-        if(alumnos.isEmpty()){
-            throw new BadRequestException("No existen alumnos");
-        }
-        return alumnos;
-    }
-
-    @PutMapping("/{id}")
-    public Persona actualizarAlumno(@PathVariable Integer id, @RequestBody Persona alumno){
-        Persona alumnoUpdate = null;
-        Optional<Persona> optionalAlumno = service.findById(id);
-        if(optionalAlumno.isEmpty()) throw new BadRequestException(String.format("Alumno con id %d no existe", id));
-        alumnoUpdate = optionalAlumno.get();
-        alumnoUpdate.setNombre(alumno.getNombre());
-        alumnoUpdate.setApellido(alumno.getApellido());
-        alumnoUpdate.setDireccion(alumno.getDireccion());
-        return service.save(alumnoUpdate);
-    }*/
-/*
-    @DeleteMapping("/{id}")
-    public void eliminarAlumno (@PathVariable Integer id){
-        alumnoDAO.deleteById(id);
-    }*/
 
     @PutMapping("/{idAlumno}/carrera/{idCarrera}")
     public ResponseEntity<?> asignarCarreraAlumno(@PathVariable Integer idAlumno, @PathVariable Integer idCarrera){
         Map<String, Object> mensaje = new HashMap<>();
         Optional<Persona> optionalAlumno = service.findById(idAlumno);
         if(optionalAlumno.isEmpty()) {
-            //throw new BadRequestException(String.format("Alumno con id %d no existe", idAlumno));
             mensaje.put("success", false);
             mensaje.put("mensaje", String.format("Alumno con id %d no existe", idAlumno));
             return ResponseEntity.badRequest().body(mensaje);
         }
         Optional<Carrera> optionalCarrera = carreraDAO.findById(idCarrera);
         if(optionalCarrera.isEmpty()) {
-            //throw new BadRequestException(String.format("La carrera con id %d no existe", idCarrera));
             mensaje.put("success", false);
             mensaje.put("mensaje", String.format("La carrera con id %d no existe", idCarrera));
             return ResponseEntity.badRequest().body(mensaje);
--- universidad-backend/src/main/java/com/springsimplespasos/universidad/universidadbackend/servicios/implementaciones/AlumnoDAOImpl.java
+++ universidad-backend/src/main/java/com/springsimplespasos/universidad/universidadbackend/servicios/implementaciones/AlumnoDAOImpl.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
 
 @Service
 public class AlumnoDAOImpl extends PersonaDAOImpl implements AlumnoDAO {
